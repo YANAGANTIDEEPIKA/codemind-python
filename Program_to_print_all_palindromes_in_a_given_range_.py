75f777ceb214bfd610df388cfad04446712d2af2
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-05-20 20:49:32 +0530

    m=int(input()) n=int(input()) for i in range(m,n+1):     t=i     r=0     while(t>0):         re=t%10         r=r*10+re         t=t//10     if(i==r):         print(r,end=' ')

--- Program_to_print_all_palindromes_in_a_given_range_.py
+++ Program_to_print_all_palindromes_in_a_given_range_.py
@@ -1,11 +1,11 @@
 m=int(input())
 n=int(input())
-for i in range(m,n+1,1):
-    r=0
+for i in range(m,n+1):
     t=i
-    while t>0:
+    r=0
+    while(t>0):
         re=t%10
-        r=(r*10)+re
+        r=r*10+re
         t=t//10
-    if(r==i):
-        print(i,end=' ')
+    if(i==r):
+        print(r,end=' ')
