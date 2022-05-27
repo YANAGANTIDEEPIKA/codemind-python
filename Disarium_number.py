64fe7599477b8b33ef77727281f5d1bf360f084c
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-05-27 21:02:32 +0530

    def count(n):     l=0     while(n!=0):         l+=1         n//=10     return l t=int(input()) n=t len=count(n) sum=0 while(n!=0):     r=n%10     sum=sum+r**len     n//=10     len-=1 if(t==sum):     print('True') else:     print('False')

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,21 +1,19 @@
-def length(t):
-    c=0
-    while(t!=0):
-        r=t%10
-        t//=10
-        c+=1
-    return c
-N=int(input())
+def count(n):
+    l=0
+    while(n!=0):
+        l+=1
+        n//=10
+    return l
+t=int(input())
+n=t
+len=count(n)
 sum=0
-t=N
-re=0
-co=length(t)
-while(t!=0):
-    r=t%10
-    sum=sum+r**co
-    t//=10
-    co-=1
-if(sum==N):
+while(n!=0):
+    r=n%10
+    sum=sum+r**len
+    n//=10
+    len-=1
+if(t==sum):
     print('True')
 else:
     print('False')
